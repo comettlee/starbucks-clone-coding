55fa7818cd5f13a4c42e578f10e8d1ef3c5b2bba
comettlee <comettlee@naver.com>
2021-08-29 00:52:06 +0900

    swiper 첫번째 슬라이드 생성

--- main.js
+++ main.js
@@ -40,3 +40,32 @@ fadeEls.forEach(function(fadeEl, index){
     opacity: 1
   })
 });
+
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical" ,
+  autoplay: {
+    delay:3000,// 하나의 슬라이드에서 다음 슬라이드 또는 이전 슬라이드 이동 시 소요 시간
+    disableOnInteraction: false
+  },
+  loop:true
+});
+
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3,  // 한번에 보여질 슬라이드 갯수
+  spaceBetween: 10,  // 슬라이드 사이 간격 (px)
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이도록 
+  loop: true,
+  autoplay: {
+    delay: 5000,// 하나의 슬라이드에서 다음 슬라이드 또는 이전 슬라이드 이동 시 소요 시간
+    disableOnInteraction: false
+  },
+  pagination:{
+    el: ".promotion .swiper-pagination" , //페이지 번호 요소 선택자
+    clickable : true
+  },
+  navigation: {
+    pervEl: ".promtion .swiper-prev",
+    nextEl: ".promotion .swiper-next"
+  }
+
+})
